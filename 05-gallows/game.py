d8daff16c1995748714c37a33b731fd020d9df79
Klindyuk <klindyuk@gmail.com>
2025-12-12 19:58:57 +0300

    занятие 8

--- 05-gallows/game.py
+++ 05-gallows/game.py
@@ -11,6 +11,27 @@ def print_frame():
     print('Ошибки:', *wrong)
     print(frames[len(wrong)])
 
+def get_user_input():
+    alf = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    while True:
+        user_input = input('Ваш ход: ').lower()
+        if len(user_input) > 1:
+            print('Нужно вводить по одной букве. Повторите ввод.')
+            continue
+        if user_input not in alf:
+            print('Слово состоит только из русских букв. Повторите ввод.')
+            continue
+        if user_input in wrong or user_input in correct:
+            print('Такая буква уже называлась. Повторите ввод.')
+            continue
+        return user_input
+
+def check_win():
+    for c in secret:
+        if c not in correct:
+            return False
+    return True
+
 frames = [
 '''
 --------
@@ -85,8 +106,15 @@ print('Компьютер загадал слово. Вам нужно его о
 game_on = True
 while game_on:
     print_frame()
-    user_input = input()
+    user_input = get_user_input()
     if user_input in secret:
         correct.append(user_input)
     else:
         wrong.append(user_input)
+    if len(wrong) == len(frames) - 1:
+        print('Вы проиграли')
+        game_on = False
+    if check_win():
+        print('Вы победили! Слово отгадано.')
+        game_on = False
+    
