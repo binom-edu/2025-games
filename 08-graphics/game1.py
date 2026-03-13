0f8b81cc55d2ef75eaf4d5e8dafa6b557e86f402
Klindyuk <klindyuk@gmail.com>
2026-03-13 18:33:45 +0300

    занятие 16

--- 08-graphics/game1.py
+++ 08-graphics/game1.py
@@ -6,6 +6,13 @@ class Pers(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.surface.Surface((10, 10))
         self.image.fill((255, 255, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center = (300, 300)
+    
+    def update(self):
+        self.rect.y += 3
+        if self.rect.bottom > 599:
+            self.rect.bottom = 599
 
 pygame.init()
 
@@ -16,7 +23,11 @@ clock = pygame.time.Clock()
 
 FPS = 30
 
+all_sprites = pygame.sprite.Group()
+
 pers = Pers()
+all_sprites.add(pers)
+
 game_on = True
 
 while game_on:
@@ -27,9 +38,11 @@ while game_on:
             game_on = False
 
     # update
+    all_sprites.update()
 
     # render
-    screen.blit(pers.image, (300, 300))
+    screen.fill((0, 0, 0))
+    all_sprites.draw(screen)
     pygame.display.flip()
 
 pygame.quit()
